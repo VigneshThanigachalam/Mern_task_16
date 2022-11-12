c0a377aecea512a4a324b165293543226d2b4727
VigneshThanigachalam <82868586+VigneshThanigachalam@users.noreply.github.com>
2022-11-12 13:45:03 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,11 @@
+document.body.classList.add("bg-info");
+var div_1= document.createElement("div");
+div_1.classList.add("w-100")
+var h_1= document.createElement("h1");
+h_1.id="display";
+h_1.classList.add("text-center");
+div_1.appendChild(h_1);
+document.body.appendChild(div_1);
 let second = 10;
 let second_display = document.getElementById('display');
 setTimeout(() => {
